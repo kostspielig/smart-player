945fbe67288d312849ed7ea5f9c114c4e368a01f
kostspielig <kostspielig@bc7bfd3f-bffe-a560-a397-c0e04b9d94c0>
2010-04-20 10:55:40 +0000

    Board+Cell arreglados

--- Board.py
+++ Board.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-
 __id__ = "$Id: Board.py $"
 __version__ = "$Revision: 1 $"
 __date__ = "$Date: 16/04/2010 Fri  $"
@@ -28,12 +27,32 @@ __)|||(_| |  |_|   | (_| / (/_ |
 
 import Cell
 
-class Board:
+class Board(object):
     
     def __init__ (self):
-        self.width = None
-        self.heith = None
+        self.__width = None
+        self.__height = None
         self.cellArray = []
+        
+    def getWidth(self):
+        return self.__width
+
+    def setWidth(self, width):
+        if isinstance(width, int) and width >= 0:
+            self.__width = width
+        else:
+            print "Error!"
+    width = property(getWidth, setWidth)
+        
+    def getHeight(self):
+        return self.__height
+
+    def setHeight(self, height):
+        if isinstance(height, int) and height >= 0:
+            self.__height = height
+        else:
+            print "Error!"
+    height = property(getHeight, setHeight)
         
     def readBoard (self, fileName):
         """ 
@@ -43,6 +62,7 @@ class Board:
             * fileName -- Path to the file containing the board description
         
         """
+
         try:
             file = open(fileName, "r")
         except IOError:
@@ -50,34 +70,60 @@ class Board:
             return -1
         
         file.readline() #Leer mapaSBT
-        cell = Cell.Cell()
    
-        self.width = int( file.readline() )
-        self.heith = int( file.readline() )
+        self.__width = int( file.readline() )
+        self.__height = int( file.readline() )
         
-        cellNumber = range(self.width * self.heith)
+        cellNumber = range(self.width * self.height)
         faceParams = range (6)
         for x in cellNumber:
+            cell = Cell.Cell()       
             cell.level = int( file.readline() )
             cell.ground = int( file.readline() )
             cell.objects = int( file.readline() )
             cell.fce = int( file.readline() )
-            cell.collapsedBuilding = bool( file.readline() )
-            cell.fire = bool( file.readline() )
-            cell.smoke = bool( file.readline() )
+            cell.collapsedBuilding = str2bool( file.readline() )
+            cell.fire = str2bool( file.readline() )
+            cell.smoke = str2bool( file.readline() )
             cell.stick = int( file.readline() )
             
             for y in faceParams:
-                cell.faceRiver.append(bool( file.readline()) )
+                cell.faceRiver.append(str2bool( file.readline()) )
             
             for y in faceParams:
-                cell.faceRoad.append(bool( file.readline()) )
+                cell.faceRoad.append(str2bool( file.readline()) )
           	
-          	self.cellArray.append(cell)
+            self.cellArray.append(cell)
+            cell = None
           
+        file.close()
                
-            
-                
-                
-            
+    def printBoard (self, filename = "out.txt"):
+        f = open (filename, "w")
+        f.write ("mapaSBT\n")
+        f.write ( str( self.__width )+"\n")
+        f.write ( str( self.__height)+"\n") 
         
+        cellNumber = range(self.__width * self.__height)
+        faceParams = range (6)
+        for x in cellNumber:
+            f.write (str( self.cellArray[x].level ) +"\n")
+            f.write (str( self.cellArray[x].ground) +"\n")
+            f.write (str( self.cellArray[x].objects) +"\n")
+            f.write (str( self.cellArray[x].fce) +"\n")
+            f.write (str( self.cellArray[x].collapsedBuilding) +"\n")
+            f.write (str( self.cellArray[x].fire) +"\n")
+            f.write (str( self.cellArray[x].smoke) +"\n")
+            f.write (str( self.cellArray[x].stick)  +"\n")
+            
+            for y in faceParams:
+                f.write (str ( self.cellArray[x].faceRiver[y]) +"\n")
+            
+            for y in faceParams:
+                f.write (str ( self.cellArray[x].faceRoad[y]) +"\n")
+          	
+        f.close()
+            
+
+def str2bool(string):
+    return string.strip().lower() in ('yes', '1', 'true')
--- Cell.py
+++ Cell.py
@@ -25,18 +25,98 @@ __)|||(_| |  |_|   | (_| / (/_ |
 """
 
 
-
-class Cell():
+class Cell(object):
     
     def __init__(self):
-        self.level = None
-        self.ground = None
-        self.objects = None
-        self.fce = None
-        self.collapsedBuilding = None
-        self.fire = None
-        self.smoke = None
-        self.stick = None
+        self.__level = None
+        self.__ground = None
+        self.__objects = None
+        self.__fce = None
+        self.__collapsedBuilding = None
+        self.__fire = None
+        self.__smoke = None
+        self.__stick = None
         self.faceRiver = []
         self.faceRoad = []
+        
+    def getLevel (self):
+        return self.__level
+
+    def setLevel (self, level):
+        if isinstance(level, int):
+            self.__level = level
+        else:
+            print "Error, incorrect level!"
+    level = property(getLevel, setLevel)
+
+        
+    def getGround (self):
+        return self.__ground
+
+    def setGround (self, ground):
+        if isinstance(ground, int) and ground >= 0:
+            self.__ground = ground
+        else:
+            print "Error, incorrect ground!"
+    ground = property(getGround, setGround)
+
+    def getObjects (self):
+        return self.__objects
+
+    def setObjects (self, objects):
+        if isinstance(objects, int) and objects >= 0:
+            self.__objects = objects
+        else:
+            print "Error, incorrect objects!"
+    objects = property(getObjects, setObjects)
+
+    def getFce (self):
+        return self.__fce
+
+    def setFce (self, fce):
+        if isinstance(fce, int) and fce >= 0:
+            self.__fce = fce
+        else:
+            print "Error, incorrect fce!"
+    fce = property(getFce, setFce)
+
+    def getCollapsedBuilding(self):
+        return self.__collapsedBuilding
+
+    def setCollapsedBuilding (self, collapsedBuilding):
+        if isinstance(collapsedBuilding, bool):
+            self.__collapsedBuilding = collapsedBuilding
+        else:
+            print "Error, incorrect collapsedBuilding!"
+    collapsedBuilding = property(getCollapsedBuilding, setCollapsedBuilding)
+
+    def getFire (self):
+        return self.__fire
+
+    def setFire (self, fire):
+        if isinstance(fire, bool):
+            self.__fire = fire
+        else:
+            print "Error, incorrect fire!"
+    fire = property(getFire, setFire)
+
+    def getSmoke (self):
+        return self.__smoke
+
+    def setSmoke (self, smoke):
+        if isinstance(smoke, bool):
+            self.__smoke = smoke
+        else:
+            print "Error, incorrect smoke!"
+    smoke = property(getSmoke, setSmoke)
+
+    def getStick (self):
+        return self.__stick
+
+    def setStick (self, stick):
+        if isinstance(stick, int) and stick >= 0:
+            self.__stick = stick
+        else:
+            print "Error, incorrect stick!"
+    stick = property(getStick, setStick)
 

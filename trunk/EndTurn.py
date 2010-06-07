4d4e28b3c36e804e5d7d3c0c7014394792cd2eaa
kostspielig <kostspielig@bc7bfd3f-bffe-a560-a397-c0e04b9d94c0>
2010-06-07 07:28:26 +0000

    Fin de Turno y Reaccion. Falta movimiento acabarlo. ya se mueve algo

---
+++ trunk/EndTurn.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+__id__ = "$Id: EndTurn.py $"
+__version__ = "$Revision: 1 $"
+__date__ = "$Date: 06/06/2010 Sat  $"
+__author__ = "Maria Carrasco Rodriguez, Francisco Manuel Herrero Perez"
+__license__ = "GPL"
+__URL__ = "http://code.google.com/p/smart-player/"
+
+
+class EndTurn:
+    """ End of Turn File"""
+
+    def __init__ (self, playerN):
+        self.playerN = playerN
+
+    def printAction(self):
+        file = open("accionJ"+ str(self.playerN)+".sbt", "w")
+        
+        file.write(str(0) +"\n")
+        file.write(str(0) +"\n")
+        file.write(str(False) +"\n")
+        file.write(str(0) +"\n")
+
+        file.close()
+
+
+
